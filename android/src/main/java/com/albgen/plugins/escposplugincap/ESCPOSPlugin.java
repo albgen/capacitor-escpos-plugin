3c23c3b3982cd713d943a0aeaa71f4c1efdddc47
evald80 <eibrahimi@smartworknet.eu>
2023-12-18 10:39:34 +0000

    ADded reference to the repo com.github.DantSu:ESCPOS-ThermalPrinter-Android:3.4.0

--- android/src/main/java/com/albgen/plugins/escposplugincap/ESCPOSPlugin.java
+++ android/src/main/java/com/albgen/plugins/escposplugincap/ESCPOSPlugin.java
@@ -1,10 +1,29 @@
 package com.albgen.plugins.escposplugincap;
 
 import android.util.Log;
+import com.dantsu.escposprinter.EscPosCharsetEncoding;
+import com.dantsu.escposprinter.EscPosPrinter;
+import com.dantsu.escposprinter.connection.DeviceConnection;
+import com.dantsu.escposprinter.connection.bluetooth.BluetoothConnection;
+import com.dantsu.escposprinter.connection.bluetooth.BluetoothConnections;
+import com.dantsu.escposprinter.connection.bluetooth.BluetoothPrintersConnections;
+import com.dantsu.escposprinter.connection.tcp.TcpConnection;
+import com.dantsu.escposprinter.connection.usb.UsbConnections;
+import com.dantsu.escposprinter.connection.usb.UsbConnections;
+import com.dantsu.escposprinter.exceptions.EscPosConnectionException;
+import com.dantsu.escposprinter.textparser.PrinterTextParserImg;
 
 public class ESCPOSPlugin {
 
     public String echo(String value) {
+        try {
+            EscPosPrinter printer = new EscPosPrinter(BluetoothPrintersConnections.selectFirstPaired(), 203, 48f, 32);
+        }
+        catch (Exception e)
+        {
+
+        }
+
         Log.i("Echo", value);
         return value;
     }
